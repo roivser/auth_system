5bbc71cac88f1a28a5f0bf2b60d2facc8db79299
Ivan <sir239@yandex.ru>
2026-05-27 19:21:19 +0300

    Подключение роутов к одной точке входа

--- app/main.py
+++ app/main.py
@@ -1,13 +1,31 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.core.database import engine
+from app.core.database import Base
+from app.api import admin, auth, business, users
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)  # Вместо Alembic
+    yield
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+app = FastAPI(
+    title="Auth & RBAC API",
+    description="Custom authentication and RBAC system",
+    lifespan=lifespan,
+)
+
+app.include_router(auth.router)
+app.include_router(users.router)
+app.include_router(admin.router)
+app.include_router(business.router)
+
+
+@app.get("/health", tags=["health"])
+async def health():
+    return {"status": "ok"}
